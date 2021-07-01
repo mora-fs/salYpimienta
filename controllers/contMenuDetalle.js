1c611f338660726c0f35f8ad7f85f9d33e0b0a70
mora-fs <mora.fernandezstickar@gmail.com>
2021-07-01 12:12:14 -0300

    imagenes dinamicas listas. la págna de error está mal configurada

--- controllers/contMenuDetalle.js
+++ controllers/contMenuDetalle.js
@@ -9,23 +9,27 @@ const controlador= {
             {
                 nombre: 'Carpaccio fresco',
                 precio: 65.50, 
+                img: 'carpaccio.jpg',
                 id: 1
             }, 
             {
                 nombre: 'Risotto de berenjena',
                 descripcion: 'Risotto de berenjena y queso de cabra',
+                img: 'risottoBerenjena.jpg',
                 precio: 47.00, 
                 id: 2
             }, 
             {
                 nombre: 'Mousse de arroz',
                 descripcion: 'Mousse de arroz con leche y aroma de azahar',
+                img: 'mousseArroz.jpg',
                 precio: 27.50, 
                 id: 3
             }, 
             {
                 nombre: 'Espárragos blancos',
                 descripcion: 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+                img: 'esparragos.png', 
                 precio: 37.50, 
                 id: 4
             }
